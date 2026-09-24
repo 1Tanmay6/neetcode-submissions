3d19c54ee78cc750863022fef95ff6538bf88b56
Tanmay Patil <93815786+1Tanmay6@users.noreply.github.com>
2026-09-24 10:41:00 +0530

    Add: two-integer-sum-ii - submission-9

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-9.py
@@ -0,0 +1,12 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        pt1 = 0
+        pt2 = len(numbers) - 1
+        while True:
+            addition = numbers[pt1] + numbers[pt2]
+            if addition == target:
+                return [pt1+1, pt2+1]
+            elif addition < target:
+                pt1 += 1
+            else:
+                pt2 -= 1
